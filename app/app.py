44d7b1e1fbf4622b02711d1867eefaacb3a39534
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-06-14 11:59:15 -0500

    merging essential and battle-ready modules from math-branch

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from matplotlib import pyplot as plt
+import numpy as np
 import time
+
+from plot import *
+from question import *
+
 
 app = Flask(__name__)
 
-@app.route('/time')
+q = Question()
+
+@app.route("/")
 def index():
     # return("test")
-    return {'time': time.time()}
+    return {"time": time.time()}
+
 
 if __name__ == "__main__":
-    index()
+    app.run(debug=True)
